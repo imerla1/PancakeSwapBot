dd08b2817259c2a3bd5b0014725b8eaa38687374
imerla1 <giorgi1048@yahoo.com>
2022-06-02 16:07:46 +0400

    added account_sid and auth_token as func parameters instead of raw variables

--- utils/call_handler.py
+++ utils/call_handler.py
@@ -1,9 +1,7 @@
 from twilio.rest import Client
 
 
-def make_phone_call(logger, from_, to):
-    account_sid = "AC4c8726d79b68ea452ad74313bbe90b9a"
-    auth_token = "94d14a624d52aa70e425a07125fbfbc4"
+def make_phone_call(account_sid,auth_token, logger, from_, to):
     client = Client(account_sid, auth_token)
 
     call = client.calls.create(
